c6e041dde32ddc4ccd60349d79b153af7585d37f
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-09 13:02:36 +0100

    Added redux store and combined reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(
+	combineReducers({
+		additionalPrice: reducers.addPriceReducer,
+		car: reducers.carReducer,
+		store: reducers.storeReducer
+	}),
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	rootElement);
--- src/state/reducers.js
+++ src/state/reducers.js
@@ -1,32 +1,36 @@
 import * as types from './actionTypes';
-const initialState = {
-	additionalPrice: 0,
-	car: {
-		price: 26395,
-		name: '2019 Ford Mustang',
-		image:
-			'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-		features: []
-	},
-	store: [
-		{ id: 1, name: 'V-6 engine', price: 1500 },
-		{ id: 2, name: 'Racing detail package', price: 1500 },
-		{ id: 3, name: 'Premium sound system', price: 500 },
-		{ id: 4, name: 'Rear spoiler', price: 250 }
-	]
+const initialAdditionalPrice = 0;
+const initialCar = {
+	price: 26395,
+	name: '2019 Ford Mustang',
+	image:
+		'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+	features: []
 };
-export const reducer = (state = initialState, action) => {
+const initialStore = [
+	{ id: 1, name: 'V-6 engine', price: 1500 },
+	{ id: 2, name: 'Racing detail package', price: 1500 },
+	{ id: 3, name: 'Premium sound system', price: 500 },
+	{ id: 4, name: 'Rear spoiler', price: 250 }
+]
+export const addPriceReducer = (state = initialAdditionalPrice, action) => {
+	switch(action.type) {
+		default:
+			return state;
+	}
+}
+export const carReducer = (state = initialCar, action) => {
+	switch(action.type) {
+		default:
+			return state;
+	}
+}
+export const storeReducer = (state = initialStore, action) => {
 	switch(action.type) {
 		case types.ADD_ITEM:
-			return {
-				...state,
-				store: [...store, action.payload]
-			}
+			return [...state, action.payload];
 		case types.REMOVE_ITEM:
-			return {
-				...state,
-				store: state.store.filter(item => item.id !== action.payload)
-			}
+			return state.store.filter(item => item.id !== action.payload);
 		default:
 			return state;
 	}
