e1bf901837d135959e66e59967bdd5a767496a0a
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-09 13:35:01 +0100

    Added localStorage to redux store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import * as storage from './helpers/localStorageState';
+import throttle from 'lodash/throttle';
 import App from './App';
 
 import 'bulma/css/bulma.css';
@@ -13,12 +15,18 @@ const store = createStore(
 		car: reducers.carReducer,
 		store: reducers.storeReducer
 	}),
+	storage.loadState(),
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+store.subscribe(throttle(() => {
+	storage.saveState(store.getState())
+}, 1000));
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
 	</Provider>,
-	rootElement);
+	rootElement
+);
